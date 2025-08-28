593b4e92de458a8faf5b95a7f775cd63187b0dae
Raydberg Gabriel <raydbergg@gmail.com>
2025-08-27 21:35:15 -0500

    first commit

---
+++ src/main/java/com/members/MsvcMembersApplication.java
@@ -0,0 +1,13 @@
+package com.members;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcMembersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsvcMembersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/members/MsvcMembersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.members;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcMembersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
