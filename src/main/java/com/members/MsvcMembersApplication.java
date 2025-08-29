eb6caa6eebb208dc9b452889b1dfa14b1c5e051e
Raydberg Gabriel <raydbergg@gmail.com>
2025-08-28 19:49:53 -0500

    configuration

--- src/main/java/com/members/MsvcMembersApplication.java
+++ src/main/java/com/members/MsvcMembersApplication.java
@@ -1,9 +1,9 @@
 package com.members;
-
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class MsvcMembersApplication {
 
     public static void main(String[] args) {
